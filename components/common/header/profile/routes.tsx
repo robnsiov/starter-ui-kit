6eb0427accc9a4ccdb6f8069ef92207ba7f1d433
robnsiov <robnsiov@gmail.com>
2023-08-17 11:45:00 +0330

    change profile routes

--- components/common/header/profile/routes.tsx
+++ components/common/header/profile/routes.tsx
@@ -3,8 +3,8 @@ import { DirectInbox, Profile, TaskSquare } from "iconsax-react";
 
 const routes: RoutesImpl = [
   { title: "Account", href: "/", icon: Profile },
-  { title: "Inbox", href: "/", icon: DirectInbox },
-  { title: "Taskboard", href: "/", icon: TaskSquare },
+  { title: "Inbox", href: "/inbox", icon: DirectInbox },
+  { title: "Taskboard", href: "/board", icon: TaskSquare },
 ];
 
 export default routes;
