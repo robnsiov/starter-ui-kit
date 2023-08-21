bc8f68fc78e97e0fa9fa6d03e70675ae95b19c99
robnsiov <robnsiov@gmail.com>
2023-08-21 23:19:23 +0330

    fix bugs

--- components/shared/inputs/auth/index.tsx
+++ components/shared/inputs/auth/index.tsx
@@ -16,7 +16,7 @@ const AuthInput = forwardRef(function AuthInput(props: AuthInputImpl, ref) {
           className={cls(
             `w-full outline-none mt-1.5 bg-indigo-100/60 dark:bg-dark-800 dark:text-zinc-200
             border-[1px] 
-             rounded-md caret-zinc-600 text-zinc-500 text-sm dark:caret-zinc-300`,
+             rounded-md caret-zinc-600 text-zinc-800 text-sm dark:caret-zinc-300`,
             props.className ? props.className : "",
             props.error ? "border-red-400" : "border-zinc-400/50",
             props.type === "password" ? "p-3 pe-14" : "p-3"
