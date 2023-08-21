df386ef48587346a30fd8d9460467d0fa94fbd0e
robnsiov <robnsiov@gmail.com>
2023-08-21 21:53:24 +0330

    fix localStorage bugs

--- utils/local-management/index.tsx
+++ utils/local-management/index.tsx
@@ -4,12 +4,9 @@ const localManagement = ({
   key,
 }: LocalManagementImpl): LocalManagementOutImpl => {
   const get =
-    typeof window !== "undefined" && typeof localStorage !== "undefined"
-      ? ""
-      : window.localStorage.getItem(key);
+    typeof window === "undefined" ? "" : window.localStorage.getItem(key);
   const set = (data: string) => {
-    if (typeof window !== "undefined" && typeof localStorage !== "undefined")
-      window.localStorage.setItem(key, data);
+    if (typeof window !== "undefined") window.localStorage.setItem(key, data);
   };
   return [get, set];
 };
