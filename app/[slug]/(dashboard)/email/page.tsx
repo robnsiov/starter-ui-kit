aebf32a8a39a97cb08499d1213e4553498ad3b01
robnsiov <robnsiov@gmail.com>
2023-08-21 18:04:54 +0330

    add dummy routes

---
+++ app/[slug]/(dashboard)/email/page.tsx
@@ -0,0 +1,29 @@
+const Page = () => {
+  return (
+    <>
+      <div>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eligendi
+        perferendis facere, iusto, quidem error laborum sit eius quis rem,
+        suscipit expedita aliquam dignissimos fugiat ratione aliquid quia sed
+        voluptatem. Tenetur? Lorem ipsum, dolor sit amet consectetur adipisicing
+        elit. Eligendi perferendis facere, iusto, quidem error laborum sit eius
+        quis rem, suscipit expedita aliquam dignissimos fugiat ratione aliquid
+        quia sed voluptatem. Tenetur? Lorem ipsum, dolor sit amet consectetur
+        adipisicing elit. Eligendi perferendis facere, iusto, quidem error
+        laborum sit eius quis rem, suscipit expedita aliquam dignissimos fugiat
+        ratione aliquid quia sed voluptatem. Tenetur? Lorem ipsum, dolor sit
+        amet consectetur adipisicing elit. Eligendi perferendis facere, iusto,
+        quidem error laborum sit eius quis rem, suscipit expedita aliquam
+        dignissimos fugiat ratione aliquid quia sed voluptatem. Tenetur? Lorem
+        ipsum, dolor sit amet consectetur adipisicing elit. Eligendi perferendis
+        facere, iusto, quidem error laborum sit eius quis rem, suscipit expedita
+        aliquam dignissimos fugiat ratione aliquid quia sed voluptatem. Tenetur?
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eligendi
+        perferendis facere, iusto, quidem error laborum sit eius quis rem,
+        suscipit expedita aliquam dignissimos fugiat ratione aliquid quia sed
+        voluptatem. Tenetur?
+      </div>
+    </>
+  );
+};
+export default Page;
